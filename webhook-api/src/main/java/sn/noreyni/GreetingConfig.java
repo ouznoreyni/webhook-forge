e3670f5124c083bc195b47a1c2217c5aac91be74
ousmane DIOP <ousmanediopp268@gmail.com>
2025-06-14 12:16:09 +0000

    initial commit

---
+++ webhook-api/src/main/java/sn/noreyni/GreetingConfig.java
@@ -0,0 +1,12 @@
+package sn.noreyni;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
